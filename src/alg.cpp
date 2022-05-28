256745c70f33b0367028df327381cdce5f41609c
Матвей Головенко <91070065+twysis@users.noreply.github.com>
2022-05-28 14:07:58 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -6,5 +6,24 @@
 #include  "bst.h"
 
 BST<std::string> makeTree(const char* filename) {
-  // поместите сюда свой код
+  BST<std::string> Tree;
+  std::string word = "";
+  std::ifstream file(filename);
+  if (!file) {
+    std::cout << "File error!" << std::endl;
+    return;
+  }
+  while(!file.eof() {
+    char ch = file.get();
+    if ((ch >= 'A' && ch <= 'Z') || ( ch >= 'a' && ch <= 'z')
+        word += ch;
+    else {
+      tolower(word);
+      Tree.add(word);
+      word = "";
+  } 
+  file.close();  
+  }
+  return Tree;      
 }
+        
